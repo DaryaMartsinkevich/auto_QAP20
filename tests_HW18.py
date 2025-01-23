219d38ea77d045bc7022889e1c8b3922c0d77908
DaryaMartsinkevich <daryamartsinkevich@gmail.com>
2025-01-23 18:24:45 +0100

    -add HW18

--- tests_HW18.py
+++ tests_HW18.py
@@ -8,11 +8,15 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
 URL = 'https://omayo.blogspot.com/'
+URL_2 = 'https://demoqa.com/webtables'
 
 
 @pytest.fixture
 def driver():
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.implicitly_wait(10)
+    driver.maximize_window()
+
     yield driver
 
     driver.close()
@@ -33,4 +37,39 @@ def test_level_2(driver):
     select.select_by_visible_text("doc 3")
     selected_option = select.first_selected_option
     assert selected_option.text == "doc 3", "Выбранная опция не установлена"
+
+
+def test_level_3(driver):
+    driver.get(URL_2)
+    add = driver.find_element(By.ID, 'addNewRecordButton')
+    add.click()
+    driver.find_element(By.ID, 'firstName').send_keys('John')
+    driver.find_element(By.ID, 'lastName').send_keys('Doe')
+    driver.find_element(By.ID, 'userEmail').send_keys('john.doe@example.com')
+    driver.find_element(By.ID, 'age').send_keys('30')
+    driver.find_element(By.ID, 'salary').send_keys('50000')
+    driver.find_element(By.ID, 'department').send_keys('IT')
+    driver.find_element(By.ID, 'submit').click()
+
+    edit = driver.find_element(By.ID, 'edit-record-4')
+    driver.execute_script('arguments[0].scrollIntoView(true)', edit)
+    time.sleep(2)
+    edit.click()
+
+    age = driver.find_element(By.ID, 'age')
+    age.clear()
+    age.send_keys('35')
+
+    salary = driver.find_element(By.ID, 'salary')
+    salary.clear()
+    salary.send_keys('55000')
+    driver.find_element(By.ID, 'submit').click()
+    time.sleep(5)
+
+    delite = driver.find_element(By.ID, 'delete-record-4')
+    driver.execute_script('arguments[0].scrollIntoView(true)', delite)
+    delite.click()
+
+
+
 
