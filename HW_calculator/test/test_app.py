a6e432bd8818ebdc16aff41cbc657f93fd376eff
DaryaMartsinkevich <daryamartsinkevich@gmail.com>
2025-02-02 16:16:17 +0100

    -add fixed HW19

--- HW_calculator/test/test_app.py
+++ HW_calculator/test/test_app.py
@@ -2,34 +2,67 @@ import pytest
 from HW_calculator.app.app import add, subtract, multiply, divide
 
 
-def test_add():
-    assert add(2, 3) == 5
-    assert add(-6, 6) == 0
-    assert add(0,0) == 0
-    assert add(-2, -3) == -5
-    assert add(2.3, 2.7) == 5
+@pytest.mark.parametrize(
+    'value_1, value_2, result',
+    [
+        (2, 3, 5),
+        (-6, 6, 0),
+        (0, 0, 0),
+        (-2, -3, -5),
+        (2.3, 2.7, 5)
+    ]
+)
+def test_add(value_1, value_2, result):
+    assert add(value_1, value_2) == result
 
 
-def test_divide():
-    assert divide(12, 2) == 6
-    assert divide(25, 5) == 5
-    assert divide(-6, -3) == 2
+@pytest.mark.parametrize(
+    'value_1, value_2, result',
+    [
+        (12, 2, 6),
+        (25, 5, 5),
+        (-6, -3, 2)
+    ]
+)
+def test_divide(value_1, value_2, result):
+    assert divide(value_1, value_2) == result
+
+
+@pytest.mark.parametrize(
+    'value_1, value_2',
+    [
+        (16, 0)
+    ]
+)
+def test_divide_error(value_1, value_2):
     with pytest.raises(ValueError):
-        divide(16, 0)
+        divide(value_1, value_2)
 
 
-def test_subtract():
-    assert subtract(10, 5) == 5
-    assert subtract(-6, 6) == -12
-    assert subtract(0, 0) == 0
-    assert subtract(-3, -3) == 0
-    assert subtract(0, 6) == -6
-    assert subtract(5.2, 1.2) == 4
+@pytest.mark.parametrize(
+    'value_1, value_2, result',
+    [
+        (10, 5, 5),
+        (-6, 6, -12),
+        (0, 0, 0),
+        (-3, -3, 0),
+        (0, 6, -6),
+        (5.2, 1.2, 4)
+    ]
+)
+def test_subtract(value_1, value_2, result):
+    assert subtract(value_1, value_2) == result
 
 
-def test_multiply():
-    assert multiply(2,3) == 6
-    assert multiply(-6, 6) == -36
-    assert multiply(2, 0) == 0
-    assert multiply(-1, -1) == 1
-    assert multiply(1.5, 2) == 3
+@pytest.mark.parametrize(
+    'value_1, value_2, result',
+    [
+        (2, 3, 6),
+        (-6, 6, -36),
+        (2, 0, 0),
+        (-1, -1, 1),
+        (1.5, 2, 3)
+    ]
+)
+def test_multiply(value_1, value_2, result):
+    assert multiply(value_1, value_2) == result
